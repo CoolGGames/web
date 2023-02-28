e68dfd9d63d5c6d48638845ac6046fac60631a2a
CoolGGames <125616753+CoolGGames@users.noreply.github.com>
2023-02-28 15:25:55 +0800

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,18 @@
+print ("If Jack has 3 apples and I take 2, how many apples does Jack have left? ")
+# assignment
+a = int(input("Number of apples: "))
+
+# conditional = if
+
+if a == 1:
+  print ("Now Jack buys 10 more apples, how many apples does Jack have now?")
+  b = int(input("Number of apples: "))
+  
+if b == 11:
+    print ("You are smart")
+
+if b != 11:
+   print ("You are really dumb")
+
+if a != 1:
+  print ("You are dumb")
